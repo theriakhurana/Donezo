4df6c9abd4392f00ecbec53138bd644207f29ebb
theriakhurana <theriakhurana@gmail.com>
2025-05-17 23:03:45 +0530

    added basic router functions

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -2,6 +2,26 @@ const express = require('express');
 const taskModel = require('../models/task');
 const router = express.Router();
 
+router.get("/", async (req, res)=>{
+  try{
+    const allTasks = await taskModel.find();
+    res.status(200).json(allTasks);
+  }
+  catch(err){
+    res.status(500).json({error : err.message});
+  }
+})
+
+router.get("/:id", async (req, res)=>{
+  try{
+    const task = await taskModel.findById(req.params.id);
+    res.status(200).json(task);
+  }
+  catch(err){
+    res.status(500).json({error : err.message});
+  }
+})
+
 router.post("/add", async (req, res)=> {
   try{
     const {title, description} = req.body;
@@ -14,6 +34,36 @@ router.post("/add", async (req, res)=> {
     // save to database
     const savedTask = await newTask.save();
     res.status(200).json(savedTask);
+  }
+  catch(err){
+    res.status(500).json({error: err.message});
+  }
+})
+
+router.delete("/:id", async (req, res)=>{
+  try{
+    const task = await taskModel.findByIdAndDelete(req.params.id);
+    if(!task){
+      return res.status(404).json({message: "Task not found"});
+    }
+    res.status(200).json({message: "Task deleted successfully"});
+  }
+  catch(err){
+    res.status(500).json({error: err.message});
+  }
+})
+
+router.put("/:id", async (req, res)=>{
+  try{
+    const {id} = req.params;
+    const task = await taskModel.findByIdAndUpdate(id, req.body);
+
+    if(!task){
+      return res.status(404).json({message: "Task not found"});
+    }
+
+    const updatedtask = await taskModel.findById(id);
+    res.status(200).json(updatedtask);
   }
   catch(err){
     res.status(500).json({error: err.message});
